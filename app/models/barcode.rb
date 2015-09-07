3d26ec68e938a02c478dbe0edfebe389e2fd0c67
Andy Weiss <andyweiss1982@gmail.com>
2015-09-07 14:54:38 -0400

    removed styles, setting width manually

--- app/models/barcode.rb
+++ app/models/barcode.rb
@@ -1,18 +1,16 @@
 class Barcode < ActiveRecord::Base
-  has_attached_file :image, 
-    styles: { medium: "300x300>", thumb: "100x100>" },
-    :storage => :s3,
-    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  has_attached_file :image,
+    storage: :s3,
+    s3_credentials: Proc.new{|a| a.instance.s3_credentials }
   validates_attachment :image, presence: true,
     content_type: { content_type: "image/jpg" }
 
   def s3_credentials
     {
-      :bucket => ENV['AWS_BUCKET_NAME'], 
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
-      :secret_access_key => ENV['AWS_SECRET_KEY']
+      bucket: ENV['AWS_BUCKET_NAME'], 
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'], 
+      secret_access_key: ENV['AWS_SECRET_KEY']
     }
   end
   
-
 end
